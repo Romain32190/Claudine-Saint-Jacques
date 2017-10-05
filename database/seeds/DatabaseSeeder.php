0593d813d2670ba2c9bd115078c692b42324b3bf
Romain32190 <romain.lafargue36@gmail.com>
2017-10-05 14:59:08 +0200

    propre

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(VehiclesSeeder::class);
         $this->call(PilgrimsSeeder::class);
+        $this->call(GitesSeeder::class);
     }
 }
