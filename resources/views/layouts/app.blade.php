c9684c4aa7985ce3ab2bcd68d2a27c210cd5d5ce
Romain32190 <romain.lafargue36@gmail.com>
2017-10-05 10:16:40 +0200

    add card

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,13 +13,13 @@
 
     <!-- Styles -->
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
-    {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
+    {{-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> --}}
 
     <style>
         body {
             font-family: 'Lato';
         }
-        .fa-btn {
+        .fa-btn {   
             margin-right: 6px;
         }
     </style>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,28 +2,22 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="col-sm-offset-2 col-sm-8">
+        <div class="col-sm-6">
             <div class="panel panel-default">
                 <div class="panel-heading">
+                    Transports Claudine
                 </div>
 
                 <div class="panel-body">
-                    <!-- Display Validation Errors -->
-
-                    <!-- New Task Form -->
                     <form action="" method="POST" class="form-horizontal">
                         {{ csrf_field() }}
 
-                        <!-- Task Name -->
                         <div class="form-group">
-                            <label for="task-name" class="col-sm-3 control-label"></label>
-
                             <div class="col-sm-6">
                             </div>
                         </div>
 
-                        <!-- Add Task Button -->
+
                         <div class="form-group">
                             <div class="col-sm-offset-3 col-sm-6">
                             </div>
@@ -39,5 +33,159 @@
                     </div>
                 </div>
         </div>
-    </div>
+         <div class="col-sm-6">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    Transports Claudine
+                </div>
+
+                <div class="panel-body">
+                    <form action="" method="POST" class="form-horizontal">
+                        {{ csrf_field() }}
+
+                        <div class="form-group">
+                            <div class="col-sm-6">
+                            </div>
+                        </div>
+
+
+                        <div class="form-group">
+                            <div class="col-sm-offset-3 col-sm-6">
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                    </div>
+
+                    <div class="panel-body">
+                    </div>
+                </div>
+        </div>
+        <div class="col-sm-6">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    Transports Claudine
+                </div>
+
+                <div class="panel-body">
+                    <form action="" method="POST" class="form-horizontal">
+                        {{ csrf_field() }}
+
+                        <div class="form-group">
+                            <div class="col-sm-6">
+                            </div>
+                        </div>
+
+
+                        <div class="form-group">
+                            <div class="col-sm-offset-3 col-sm-6">
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                    </div>
+
+                    <div class="panel-body">
+                    </div>
+                </div>
+        </div>
+        <div class="col-sm-6">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    Transports Claudine
+                </div>
+
+                <div class="panel-body">
+                    <form action="" method="POST" class="form-horizontal">
+                        {{ csrf_field() }}
+
+                        <div class="form-group">
+                            <div class="col-sm-6">
+                            </div>
+                        </div>
+
+
+                        <div class="form-group">
+                            <div class="col-sm-offset-3 col-sm-6">
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                    </div>
+
+                    <div class="panel-body">
+                    </div>
+                </div>
+        </div>
+        <div class="col-sm-6">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    Transports Claudine
+                </div>
+
+                <div class="panel-body">
+                    <form action="" method="POST" class="form-horizontal">
+                        {{ csrf_field() }}
+
+                        <div class="form-group">
+                            <div class="col-sm-6">
+                            </div>
+                        </div>
+
+
+                        <div class="form-group">
+                            <div class="col-sm-offset-3 col-sm-6">
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                    </div>
+
+                    <div class="panel-body">
+                    </div>
+                </div>
+        </div>
+        <div class="col-sm-6">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    Transports Claudine
+                </div>
+
+                <div class="panel-body">
+                    <form action="" method="POST" class="form-horizontal">
+                        {{ csrf_field() }}
+
+                        <div class="form-group">
+                            <div class="col-sm-6">
+                            </div>
+                        </div>
+
+
+                        <div class="form-group">
+                            <div class="col-sm-offset-3 col-sm-6">
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                    </div>
+
+                    <div class="panel-body">
+                    </div>
+                </div>
+        </div>
 @endsection
