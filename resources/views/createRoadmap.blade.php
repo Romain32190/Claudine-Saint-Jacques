84dc79df4c0178901149c72b6aa32f3a1e9d2d75
Romain32190 <romain.lafargue36@gmail.com>
2017-10-09 12:12:04 +0200

    add drag and drop

--- resources/views/createRoadmap.blade.php
+++ resources/views/createRoadmap.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-  <div class="col-sm-2" style="overflow:scroll; scrollbar force-overflow; height:100%;">
+  <div class="col-sm-2"  style="overflow:scroll; scrollbar force-overflow; height:100%;">
 
     @foreach ($luggages as $luggage)
-      <div class="panel panel-default">
+      <div class="panel panel-default box navy" id="box1" draggable="true">
         <div class="panel-heading" style="background-color: #0000FF; color:white;">
           Bagages
         </div>
@@ -33,7 +33,7 @@
 <div class="col-sm-2" style="overflow:scroll; scrollbar force-overflow; height:100%;">
 
   @foreach ($pilgrims as $pilgrim)
-    <div class="panel panel-default">
+    <div class="panel panel-default box red" id="box2" draggable="true">
       <div class="panel-heading" style="background-color: #0000FF; color:white;">
         {{$pilgrim->name}}
       </div>
@@ -44,17 +44,16 @@
 <div class="col-sm-2" style="overflow:scroll; scrollbar force-overflow; height:100%;">
 
       @foreach ($drivers as $driver)
-        <div class="panel panel-default">
-          <div class="panel-heading" style="background-color: #0000FF; color:white;">
+        <div draggable="true" id="box3" class="panel panel-default box green">
+          <div class="panel-heading"  style="background-color: #0000FF; color:white;">
             {{$driver->name}}
           </div>
       </div>
       @endforeach
 
 </div>
+<div data-drop-target="true" style="margin-left: 42em; height : 50em; width: 45em;"></div>
 </div>
 
-<script async defer
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBkOzOLLwUvc8nJvyTm2KJPNFD6bPDKtWk&callback=initMap">
-    </script>
+
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,16 +1,18 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
   @include('layouts.head')
 
     <body id="app-layout">
       @include('layouts.navbar')
-
-      <div class="container" style="height: 100vh;">
+      <div style="height: 100vh;">
+      <div data-drop-target="true"></div>
         @yield('content')
       </div>
+      
 
 
     <!-- JavaScripts -->
     @include('layouts.footer')
   </body>
+  
 </html>
