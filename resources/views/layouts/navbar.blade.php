a37a9f86216d05115cd88f1012bf01cdf591e876
Romain32190 <romain.lafargue36@gmail.com>
2017-10-09 14:50:36 +0200

    design css

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -3,12 +3,12 @@
         <div class="navbar-header">
 
             <!-- Branding Image -->
-            <a class="navbar-brand" href="{{ url('/') }}" style="color: white;">
+            <a class="navbar-brand" href="{{ url('/') }}" style="color: white; margin-top:0.0.1em;">
                 {{config('app.name')}}
             </a>
            <ul class="nav navbar-nav">
-             <li><a href="/carte" style="font-size: 16px; color: black;">Carte</a></li>
-             <li><a href="/createroadmap">Feuille de route</a></li>
+             <li><a href="/carte" style="font-size: 16px; color: white; margin-top:0.1em;">Carte</a></li>
+             <li><a href="/createroadmap" style="font-size: 16px; color: white; margin-top:0.1em;">Feuille de route</a></li>
              <li><a href="#"></a></li>
              <li><a href="#"></a></li>
           </ul>
