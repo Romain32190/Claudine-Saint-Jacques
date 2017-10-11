a08c4bb206582db41b2af038636312e830dd70c9
Romain32190 <romain.lafargue36@gmail.com>
2017-10-11 10:37:17 +0200

    add dropdown

--- resources/views/createRoadmap.blade.php
+++ resources/views/createRoadmap.blade.php
@@ -2,18 +2,19 @@
 
 @section('content')
   <div class="col-sm-2"  style="overflow:scroll; scrollbar force-overflow; height:100%;">
-
+    <div class="dropdown">
+      <button class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown" style="width: 10em;"><i class="fa fa-suitcase" aria-hidden="true"></i>
+        <span class="caret"></span></button>
+        <ul class="dropdown-menu">
     @foreach ($luggages as $luggage)
       <div class="panel panel-default box navy" id="luggage-{{$luggage->id}}" draggable="true">
-        <div class="panel-heading" style="background-color: #0000FF; color:white;">
-          Bagages
+        <div class="panel-heading" id="luggage-{{$luggage->id}}" draggable="true" style="background-color: #0000FF; color:white;">
+          {{$luggage->pilgrim['name']}}
         </div>
 
 
         <div class="panel-body">
 
-
-          <h3>{{$luggage->pilgrim['name']}}</h3>
           <p>{{$luggage->start}}</p><p>{{$luggage->goal}}</p><br>
           <p>{{$luggage->weight}} kg</p>
 
@@ -22,20 +23,30 @@
       </div>
       </div>
     @endforeach
+  </ul>
+  </div>
 </div>
 
 <div class="col-sm-2" style="overflow:scroll; scrollbar force-overflow; height:100%;">
 
+  <div class="dropdown">
+    <button class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown" style="width: 10em;"><i class="fa fa-users" aria-hidden="true"></i>
+      <span class="caret"></span></button>
+      <ul class="dropdown-menu">
   @foreach ($pilgrims as $key => $pilgrim)
-    <div class="panel panel-default box red" id="pilgrim-{{$pilgrim->id}}" draggable="true">
-      <div class="panel-heading" style="background-color: #0000FF; color:white;">
-        {{$pilgrim->name}}
-      </div>
-    </div>
+  <div class="panel-heading" draggable="true" id="pilgrims-{{$pilgrim->id}}"  style="background-color: #0000FF; color:white; width:12em;">
+    {{$pilgrim->name}}
+  </div>
   @endforeach
+</ul>
+</div>
 </div>
 
 <div class="col-sm-2" style="overflow:scroll; scrollbar force-overflow; height:100%;">
+  <div class="dropdown">
+    <button class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown" style="width: 10em;"><i class="fa fa-truck" aria-hidden="true"></i>
+      <span class="caret"></span></button>
+      <ul class="dropdown-menu">
 
       @foreach ($drivers as $key => $driver)
         <div draggable="true" id="drivers-{{$driver->id}}" class="panel panel-default box green">
@@ -44,6 +55,8 @@
           </div>
       </div>
       @endforeach
+    </ul>
+    </div>
 
 </div>
 <div style="display:flex;">
