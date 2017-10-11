d58ed4ec4885285928a497ac707c82e0a8289229
Romain32190 <romain.lafargue36@gmail.com>
2017-10-11 14:38:01 +0200

    propre

--- resources/views/createRoadmap.blade.php
+++ resources/views/createRoadmap.blade.php
@@ -1,75 +1,51 @@
 @extends('layouts.app')
 
 @section('content')
-  <div class="col-sm-2"  style="overflow:scroll; scrollbar force-overflow; height:100%;">
-    <div class="dropdown">
-      <button class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown" style="width: 10em;"><i class="fa fa-suitcase" aria-hidden="true"></i>
-        <span class="caret"></span></button>
-        <ul class="dropdown-menu">
+<div  style="width : 25%; display:flex;">
+  <div class="col-sm-2">
+        <ul style="display:flex;" >
     @foreach ($luggages as $luggage)
-      <div class="panel panel-default box navy" id="luggage-{{$luggage->id}}" draggable="true">
-        <div class="panel-heading" id="luggage-{{$luggage->id}}" draggable="true" style="background-color: #0000FF; color:white;">
-          {{$luggage->pilgrim['name']}}
+        <div  id="luggage-{{$luggage->id}}" draggable="true" style="line-height: 2em; color:white;">
+          <a href="#" data-toggle="tooltip" title="{{$luggage->pilgrim['name']}}" style="color:black;"><i class="fa fa-suitcase" aria-hidden="true" style="margin-left: 0.8em;"></i></a>
         </div>
-
-
-        <div class="panel-body">
-
-          <p>{{$luggage->start}}</p><p>{{$luggage->goal}}</p><br>
-          <p>{{$luggage->weight}} kg</p>
-
-
-           </p>
-      </div>
-      </div>
     @endforeach
-  </ul>
-  </div>
-</div>
+    </ul>
 
-<div class="col-sm-2" style="overflow:scroll; scrollbar force-overflow; height:100%;">
-
-  <div class="dropdown">
-    <button class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown" style="width: 10em;"><i class="fa fa-users" aria-hidden="true"></i>
-      <span class="caret"></span></button>
-      <ul class="dropdown-menu">
+      <ul style="display:flex;"  >
   @foreach ($pilgrims as $key => $pilgrim)
-  <div class="panel-heading" draggable="true" id="pilgrims-{{$pilgrim->id}}"  style="background-color: #0000FF; color:white; width:12em;">
-    {{$pilgrim->name}}
+  <div  draggable="true" id="pilgrims-{{$pilgrim->id}}"  style="line-height: 2em;">
+    <a href="#" data-toggle="tooltip" title="{{$pilgrim->name}}" style="color:black;"><i class="fa fa-users" aria-hidden="true" style="margin-left: 0.8em;"></i></a>
   </div>
   @endforeach
 </ul>
-</div>
-</div>
 
-<div class="col-sm-2" style="overflow:scroll; scrollbar force-overflow; height:100%;">
-  <div class="dropdown">
-    <button class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown" style="width: 10em;"><i class="fa fa-truck" aria-hidden="true"></i>
-      <span class="caret"></span></button>
-      <ul class="dropdown-menu">
+      <ul style="display:flex;">
 
       @foreach ($drivers as $key => $driver)
-        <div draggable="true" id="drivers-{{$driver->id}}" class="panel panel-default box green">
-          <div class="panel-heading"  style="background-color: #0000FF; color:white;">
-            {{$driver->name}}
+          <div   draggable="true" id="drivers-{{$driver->id}}" style="line-height: 2em;" >
+            <a href="#" data-toggle="tooltip" title="{{$driver->name}}" style="color:black;"><i class="fa fa-truck" aria-hidden="true" style="margin-left: 0.8em;"></i></a>
           </div>
-      </div>
       @endforeach
     </ul>
-    </div>
+  </div>
+</div>
+<!-- <div style="display:flex;">
+<div data-drop-target="true" style="height : 39em; width: 10em;"></div>
+<div data-drop-target="true" style="height : 39em; width: 10em;"></div>
+<div data-drop-target="true" style="height : 39em; width: 10em;"></div>
+<div data-drop-target="true" style="height : 39em; width: 10em;"></div>
+</div> -->
+<!-- </div> -->
 
-</div>
-<div style="display:flex;">
-<div data-drop-target="true" style="height : 39em; width: 10em;"></div>
-<div data-drop-target="true" style="height : 39em; width: 10em;"></div>
-<div data-drop-target="true" style="height : 39em; width: 10em;"></div>
-<div data-drop-target="true" style="height : 39em; width: 10em;"></div>
-</div>
-</div>
 
 
 @endsection
 
 @section('script')
   <script src={{asset('js/main.js')}} type="text/javascript"></script>
+  <script>
+  $(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();
+});
+</script>
 @endsection
