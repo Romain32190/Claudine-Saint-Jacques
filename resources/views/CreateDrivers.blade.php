39aee1726d6fab7f247c0d78d35650be3dee5403
Romain32190 <romain.lafargue36@gmail.com>
2017-10-12 09:39:22 +0200

    propre

--- resources/views/CreateDrivers.blade.php
+++ resources/views/CreateDrivers.blade.php
@@ -2,55 +2,61 @@
 
 @section('content')
 
+<div class="container">
   <form class="" action="{{route('drivers.store')}}" method="POST">
     {{csrf_field()}}
+    <div class="row">
+    <div class="col-sm-6">
 
     <label for="name">Nom</label>
-    <input type="text" name="name" id="name">
+    <input class="form-control" type="text" name="name" id="name">
 
     <label for="firstname">Prénom</label>
-    <input type="text" name="firstname" id="firstanme"><br>
+    <input class="form-control" type="text" name="firstname" id="firstanme"><br>
 
     <label for="drivingLicense">N° Permis de conduire</label>
-    <input type="text" name="drivingLicence" id="drivingLicence"><br>
+    <input class="form-control" type="text" name="drivingLicence" id="drivingLicence"><br>
 
     <label for="number">N°</label>
-    <input type="text" name="number" id="number">
+    <input class="form-control" type="text" name="number" id="number">
 
-    <label for="street">rue</label>
-    <input type="text" name="street" id="street"><br>
+    <label for="street">Rue</label>
+    <input class="form-control" type="text" name="street" id="street"><br>
 
-    <label for="flatnumber">appartement n°</label>
-    <input type="text" name="flatnumber" id="flatnumber">
+    <label for="flatnumber">Appartement n°</label>
+    <input class="form-control" type="text" name="flatnumber" id="flatnumber">
 
+  </div>
+    <div class="col-sm-6">
     <label for="floor">Etage</label>
-    <input type="text" name="floor" id="floor" >
+    <input class="form-control" type="text" name="floor" id="floor" >
 
     <label for="flatName">Batiment</label><br>
-    <input type="text" name="flatName" id="flatName">
+    <input class="form-control" type="text" name="flatName" id="flatName">
 
-    <label for="zipcode">code postal</label>
-    <input type="text" name="zipcode" id="zipcode">
+    <label for="zipcode">Code postal</label>
+    <input class="form-control" type="text" name="zipcode" id="zipcode">
 
     <label for="city">Ville</label><br>
-    <input type="text" name="city" id="city" >
+    <input class="form-control" type="text" name="city" id="city" >
 
     <label for="phone">Téléphone fixe</label>
-    <input type="text" name="phone" id="phone">
+    <input class="form-control" type="text" name="phone" id="phone">
 
     <label for="mobile">Portable</label>
-    <input type="text" name="mobile" id="mobile"><br>
+    <input class="form-control" type="text" name="mobile" id="mobile"><br>
 
     <label for="vehicle">Véhicule</label>
-    <select class="" name="vehicle" id="vehicle">
+    <select class="form-control" name="vehicle" id="vehicle">
       @foreach ($vehicles as $vehicle)
       <option value="{{$vehicle->id}}">{{$vehicle->type}}:{{$vehicle->vehicleRegistration}}</option>
       @endforeach
     </select><br>
 
 
-    <button type="submit">créer</button>
-
+    <button class="btn btn-primary"type="submit">Créer</button>
+  </div>
+</div>
   </form>
 
 @endsection
